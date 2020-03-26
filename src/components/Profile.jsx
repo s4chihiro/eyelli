c063a4de1792f8dc7ee40e5e397b2fb905a45166
s4chihiro <tikhonov.ac@yandex.ru>
2020-03-27 00:09:52 +0300

    added components (Header, Navbar, Profile)

---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+      <div className="content">
+        <div>
+          <img src="https://asiamountains.net/upload/slide/slide-1960x857-07.jpg"/>
+        </div>
+        <div>ava + discription</div>
+        <div>
+          My posts
+          <div>New posts</div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
+    );
+}
+
+export default Profile;
